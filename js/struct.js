e3522f53227b3bea5ff05c94d160d59943e70210
Jerzy Drożdż <jdrozdz@intershock.pl>
2020-12-17 16:58:00 +0100

    obliczanie pola kwadratu wersja 2

--- js/struct.js
+++ js/struct.js
@@ -10,5 +10,10 @@ function Calculate() {
     const a = document.querySelector("#a").value;
     const b = document.querySelector("#b").value;
     const result = document.querySelector("#result");
-    result.innerHTML = SquereCalctulator(a, b);
+    if (isNaN(a) || isNaN(b) || a === 0 
+        || b === 0 || b.length === 0 || a.length === 0) {
+        result.innerHTML = "<b style='color: white; background-color: red'>Podaj liczbe wieksza od 0</b>";
+    } else {
+        result.innerHTML = SquereSurfaceArea(a, b);
+    }
 }
